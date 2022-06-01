1559d106fd6e5d01ab7bdd1042c5b967b709473e
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-06-01 03:08:48 -0400

    Get Route for all players with knex query for certain data to be retrieved

---
+++ controllers/teamController.js
@@ -0,0 +1,12 @@
+const knex = require('knex')(require('../knexfile'));
+
+/* [ROUTE] - '/team' */
+// [GET] - Retrieves all players info, Querying for name, image, position, PPG, VI, RPG, eFG%
+exports.index = (_req, res) => {
+    knex('players')
+    .select('playerNumber', 'name', 'image', 'position', 'PPG', 'VI', 'RPG', 'eFG%' )
+    .then((data) => {
+        res.status(200).json(data);
+    })
+    .catch((err) => res.status(400).send(`Error retrieving team data ${err}`));
+};
